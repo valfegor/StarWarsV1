f72648262c7c292b0c985462cc6a1cfb5f61e3a8
Anthony Ahumada <locopala99@gmail.com>
2021-08-04 14:46:24 +0200

    more js

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,6 +17,8 @@ const obtener = (nuevoarray)=>{
     let html = "";
     document.getElementById("datos_personajes").innerHTML="";
     nuevoarray.forEach(element => {
+      
+        
         return fetch(element)
         .then((response)=> response.json())
         .then((json)=>{
@@ -26,16 +28,30 @@ const obtener = (nuevoarray)=>{
 }
 
 const pintar = (data,html)=>{
+    let genero = "";
+    
+    if(data.gender=="female"){
+        genero="Femenino"
+    }
+    else if(data.gender=="male"){
+        genero="Masculino"
+    }
+    else{
+        genero="Que rayos es esto!"
+    }
+    console.log(genero);
+    
     html += `<div class="card">`
     html += `<h4>Nombre : ${data.name}</h4>`
     html += `<p>Masa : ${data.mass}</p>` 
     html += `<p>Color de cabello : ${data.hair_color}</p>`
     html += `<p>Color de piel : ${data.skin_color}</p>`
     html += `<p>Color de ojos : ${data.eye_color}</p>` 
-    html += `<p>Genero : ${data.gender}</p>`  
+    html += `<p>Genero : ${genero}</p>`  
     html += `</div>`
     
-      
+     
+    
 
     
 
